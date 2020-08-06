61e50d39fa05147cbf5fd13765c3c413e70feb37
Parvish Gajjar <46929178+ParvishGajjar@users.noreply.github.com>
2020-08-06 17:28:51 +0530

    Add files via upload

---
+++ arraytodictnew.js
@@ -0,0 +1,12 @@
+var arr=[{name: 'parvish',maths: 50, science: 63, social: 81},{name: 'nishi',maths: 70, science: 63, social:76}];
+
+var newarr=arr.map(Func);
+console.log(newarr)
+function Func(item){
+		var dict={}
+		var n=item.name;
+		var v=item.maths+item.science+item.social;
+		dict[n]=v
+		console.log(dict)
+		return dict
+}
