d18e8a0c37d710c1937a9f524a43bc41eff4e65f
Parvish Gajjar <46929178+ParvishGajjar@users.noreply.github.com>
2020-08-07 15:32:00 +0530

    Add files via upload

---
+++ ternary.js
@@ -0,0 +1,21 @@
+//Ternary Operators
+var a=10,b=20;
+//Ternary Operator Basics
+var c=(a>b)?'True':'False' //syntax: (condition)?True(if found true returs this):False(if found false returns this)
+console.log(c)
+
+//Nested Ternary Operator
+var d=(a<b)?((a==5)?"A is 5":"A is other than 5"):"B"
+console.log(d) 
+
+//Ternary Operator with functions
+function aisgreat(){
+	return "A is greater than B";
+}
+function bisgreat(){
+	return "B is greater than A";
+}
+
+var e=(a>b)?aisgreat():bisgreat();
+console.log(e);
+
