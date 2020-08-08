1cf9baca7c6062cb68b3e771f2ae006bf651b60a
Parvish Gajjar <46929178+ParvishGajjar@users.noreply.github.com>
2020-08-08 16:18:50 +0530

    Callback Function

---
+++ callback-new.js
@@ -0,0 +1,17 @@
+//Synchronised 
+var fs = require("fs");
+var data = fs.readFileSync('input.txt');
+
+console.log(data.toString());
+console.log("Program Ended1");
+
+//Callback function included
+
+var fs = require("fs");
+
+fs.readFile('input.txt', function (err, data2) {
+   if (err) return console.error(err);
+   console.log(data2.toString());
+});
+
+console.log("Program Ended2");
